e0bb3eb772f0c998f45c4999f7eb03b0ec3276f6
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-05-21 15:54:37 -0400

    add write result interface

--- taskdb/taskdb.go
+++ taskdb/taskdb.go
@@ -4,22 +4,22 @@ import "time"
 
 type TaskDB interface {
 	// Acquires the TaskDB lock as needed, then returns an AttachIterator
-	Attach() (AttachIterator, error)
+	Attach() AttachIterator
 
 	// A Task will be inserted into the task table, and its first state inserted
-	Enqueue(state *TaskDBTaskState, payload []byte) error
+	Enqueue(state *TaskDBTaskState, payload []byte) WriteResult
 
 	// A new task state
-	PutState(state *TaskDBTaskState) error
+	PutState(state *TaskDBTaskState) WriteResult
 
 	// Retrieves the payload for a given task
 	GetPayload(topicName, taskID string) ([]byte, error)
 
 	// Deletes all task states for a topic, and removes the topic from the task table. If no more tasks exist then the task will be removed from the task table
-	Delete(topicName, taskID string) error
+	Delete(topicName, taskID string) WriteResult
 
-	// Returns an interator that will read all payloads from the DB, so they can be drained into other partitions
-	Drain() (DrainIterator, error)
+	// Returns an iterator that will read all payloads from the DB, so they can be drained into other partitions
+	Drain() DrainIterator
 }
 
 type TaskDBTaskState struct {
@@ -61,4 +61,13 @@ type DrainTask struct {
 type DrainIterator interface {
 	// Returns an array of task payloads that can be joined to their current states, and sent to other partitions. Returns nil when there are no more
 	Next() ([]*DrainTask, error)
+}
+
+type WriteResult interface {
+	// Waits for the write to be committed to the TaskDB.
+	// Should use a buffered channel to wait for a batch to be committed.
+	// If the TaskDB does not batch it should still immediately write to
+	// the buffered channel so that function calls are non-blocking and
+	// return immediately
+	Get() error
 }
