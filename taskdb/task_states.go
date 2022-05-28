68015545d6b125e1ef2b3b6ed57cbbb8ad032b0b
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-05-28 08:32:31 -0400

    remove unused task states

--- taskdb/task_states.go
+++ taskdb/task_states.go
@@ -8,8 +8,6 @@ const (
 	TASK_STATE_ENQUEUED TaskState = 0
 	TASK_STATE_DELAYED  TaskState = 1
 	TASK_STATE_INFLIGHT TaskState = 2
-	TASK_STATE_ACKED    TaskState = 3
-	TASK_STATE_NACKED   TaskState = 4
 )
 
 func (ts TaskState) String() string {
@@ -20,10 +18,6 @@ func (ts TaskState) String() string {
 		return "delayed"
 	case TASK_STATE_INFLIGHT:
 		return "inflight"
-	case TASK_STATE_ACKED:
-		return "acked"
-	case TASK_STATE_NACKED:
-		return "nacked"
 	default:
 		return fmt.Sprintf("%d", int(ts))
 	}
